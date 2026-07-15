2ed305495754692ab66532d42fe743da78cb3498
joao paulo <joaopauloexcel@hotmail.com>
2026-07-14 21:42:58 -0300

    feat: add actions deploy

--- Mechanic/terraform/provider.tf
+++ Mechanic/terraform/provider.tf
@@ -1,3 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  count      = var.is_local ? 0 : 1
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  count      = var.is_local ? 0 : 1
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
 provider "kubernetes" {
-  config_path = var.kubeconfig_path
+  # Modo Local (Minikube / Docker Desktop)
+  config_path = var.is_local ? var.kubeconfig_path : null
+
+  # Modo Nuvem (AWS EKS)
+  host                   = var.is_local ? null : data.aws_eks_cluster.cluster[0].endpoint
+  cluster_ca_certificate = var.is_local ? null : base64decode(data.aws_eks_cluster.cluster[0].certificate_authority.data)
+  token                  = var.is_local ? null : data.aws_eks_cluster_auth.cluster[0].token
 }
--- Mechanic/terraform/variables.tf
+++ Mechanic/terraform/variables.tf
@@ -1,5 +1,10 @@
-variable "kubeconfig_path" {
-  description = "Caminho para o arquivo kubeconfig"
-  type        = string
-  default     = "~/.kube/config"
+variable "is_local" {
+  type        = bool
+  description = "Define se o deploy é local (true) ou na AWS (false)"
+  default     = true # Por padrão, roda local na sua máquina
 }
+
+variable "kubeconfig_path" {
+  type        = string
+  description = "Caminho do arquivo kubeconfig para o ambiente local"
+  default     = "~/.kube/config"
